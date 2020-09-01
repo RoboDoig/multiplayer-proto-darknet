865236b66e081078cb17fb68e2e8b9bb886e6fa2
Andrew Erskine <erskine94@gmail.com>
2020-08-31 22:17:20 -0700

    Basic network comms

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DarkRift;
+using DarkRift.Client;
+using DarkRift.Client.Unity;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    UnityClient client;
+
+    void Awake() {
+        if (client == null) {
+            Debug.Log("Client unassigned in SpawnManager");
+            Application.Quit();
+        }
+
+        client.MessageReceived += MessageReceived;
+    }
+
+    void MessageReceived(object sender, MessageReceivedEventArgs e) {
+        using (Message message = e.GetMessage())
+        using (DarkRiftReader reader = message.GetReader()) {
+
+            ushort id = reader.ReadUInt16();
+
+            Debug.Log(id);
+
+        }
+    }
+}
---
+++ ProtoPlugin/ProtoPlugin/PlayerManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DarkRift;
+using DarkRift.Server;
+
+namespace ProtoPlugin
+{
+    public class PlayerManager : Plugin
+    {
+        public override bool ThreadSafe => false;
+
+        public override Version Version => new Version(0, 0, 1);
+
+        public PlayerManager(PluginLoadData pluginLoadData) : base(pluginLoadData)
+        {
+            ClientManager.ClientConnected += ClientConnected;
+            ClientManager.ClientDisconnected += ClientDisconnected;
+        }
+
+        void ClientConnected(object sender, ClientConnectedEventArgs e)
+        {
+            // When client connects, send its ID back to it
+            using (DarkRiftWriter newWriter = DarkRiftWriter.Create())
+            {
+                newWriter.Write(e.Client.ID);
+
+                using (Message newMessage = Message.Create(Tags.TestMessageTag, newWriter))
+                {
+                    foreach (IClient client in ClientManager.GetAllClients().Where(x => x == e.Client))
+                        client.SendMessage(newMessage, SendMode.Reliable);
+                }
+
+            }
+        }
+
+        void ClientDisconnected(object sender, ClientDisconnectedEventArgs e)
+        {
+
+        }
+    }
+}
