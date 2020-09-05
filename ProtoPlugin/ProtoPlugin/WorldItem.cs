9e24b63d452f1a741e24bca99f30e76fa279f6f3
Andrew Erskine <erskine94@gmail.com>
2020-09-04 23:47:58 -0700

    Some notes

---
+++ ProtoPlugin/ProtoPlugin/WorldItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoPlugin
+{
+    public class WorldItem
+    {
+        public string name;
+        public string description;
+        public float weight;
+
+        public class Resource
+        {
+
+        }
+    }
+}
